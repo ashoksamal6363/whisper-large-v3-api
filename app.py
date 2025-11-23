3df6423c5fbdd46dc9533dc7701dc17815f5fff6
Ashok Samal <assamal@win.redhat.com>
2025-11-23 17:10:57 +0400

    Add Llama auth header

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ model = whisper.load_model(WHISPER_MODEL_NAME, device=device)
 
 LLAMA_URL = os.getenv(
     "LLAMA_URL",
-    "https://redhataillama-31-8b-instruct3-llama-stack.apps.cluster-gltrd.gltrd.sandbox2574.opentlc.com/v1/chat/completions",
+    "https://redhataillama-31-8b-instruct3-llama-stack.apps.cluster-gltrd.gltrd.sandbox2574.opentlc.com,
 )
 LLAMA_TOKEN = os.getenv("LLAMA_TOKEN", "sha256~5DlvevZJury0P0CMJddlK2yNPgt9Qq9lSTmrLr7EJ0w")  # set this in the deployment
 LLAMA_MODEL = os.getenv("LLAMA_MODEL", "redhataillama-31-8b-instruct3")
